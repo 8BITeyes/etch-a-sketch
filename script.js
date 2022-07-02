38f1b08acc3d55bf410981586ba2b7e54e7eb446
RyanFrazin <RyanFrazin@Gmail.com>
2022-07-01 22:16:10 -0700

    Ryan Frazin Jul 1, 2022 22:16
    Add element  button to DOM.
    
    Button now prompts user to input a number between 1 and 100.
    
    Grid stays at a height and width of 960px, but changes amount of columns
    and rows based on user input.

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 const body = document.querySelector("body");
 
-
 const container = document.querySelector(".container");
-container.classList.add('content');
+container.classList.add('grid');
 
-let totalBoxes = 256;
-for (let i = 0; i < totalBoxes; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    container.appendChild(div);
+const createDiv = (number) => {
+    let totalBoxes = (number * number);
+    for (let i = 0; i < totalBoxes; i++) {
+        const div = document.createElement('div');
+        div.classList.add('box');
+        container.appendChild(div);
+        
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "black";
+        });
+    };
 }
+createDiv(16);
 
-const boxes = container.querySelectorAll('div');
-boxes.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = "black";
-    });
-});
-
-//Asks user for a number of boxes to change the container to.
+//Button asks user for a number of boxes to change the container to when pressed.
 const button = document.createElement('button');
 body.prepend(button);
 button.textContent = "Click me";
-button.addEventListener('click', () => {
-    let number = prompt("How many squares per side?");
-});
+
+function userPrompt() {
+    let number = prompt("Enter a number between 1 and 100");
+    while (number < 1 || number > 100 || number === NaN) {
+        number = prompt("Invalid number. Enter a number between 1 and 100");
+    }
+    return number;
+}
+
+const newGrid = () => {
+    let number = userPrompt();
+
+    container.replaceChildren();
+    container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    
+    createDiv(number);
+}
+
+button.addEventListener('click', newGrid);
