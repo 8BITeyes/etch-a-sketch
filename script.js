06452a5c49bcc05f289932268549558f8bcfd694
RyanFrazin <RyanFrazin@Gmail.com>
2022-07-02 21:27:04 -0700

    Ryan Frazin Jul 2, 2022 21:27
    Add button container to index body.
    
    Add new buttons that change background color of divs to either black
    or random.
    
    Add mode to top of script that changes based on what button is pressed.
    
    Add color function that generates random color for divs.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const body = document.querySelector("body");
+const buttonContainer = document.querySelector(".buttoncontainer");
 
 const container = document.querySelector(".container");
 container.classList.add('grid');
+
+let mode = 1;
 
 const createDiv = (number) => {
     let totalBoxes = (number * number);
@@ -9,18 +12,28 @@ const createDiv = (number) => {
         const div = document.createElement('div');
         div.classList.add('box');
         container.appendChild(div);
-        
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = "black";
-        });
-    };
+
+        switch (mode) {
+            case (2):
+                div.addEventListener('mouseover', () => {
+                    div.style.backgroundColor = color();
+                })
+            break;
+
+            default:
+                div.addEventListener('mouseover', () => {
+                    div.style.backgroundColor = "black";
+                })
+            break;
+        }
+    }            
 }
 createDiv(16);
 
 //Button asks user for a number of boxes to change the container to when pressed.
-const button = document.createElement('button');
-body.prepend(button);
-button.textContent = "Click me";
+const squareButton = document.createElement('button');
+buttonContainer.appendChild(squareButton);
+squareButton.textContent = "Squares";
 
 function userPrompt() {
     let number = prompt("Enter a number between 1 and 100");
@@ -40,4 +53,34 @@ const newGrid = () => {
     createDiv(number);
 }
 
-button.addEventListener('click', newGrid);
+squareButton.addEventListener('click', newGrid);
+
+//Party mode changes divs background colors to random. Boring mode changes divs bg color back to black.
+const partyButton = document.createElement('button');
+buttonContainer.prepend(partyButton);
+partyButton.textContent = "Party Mode";
+
+partyButton.addEventListener('click', () => {
+    mode = 2;
+    newGrid();
+});
+
+
+const boringButton = document.createElement('button');
+buttonContainer.prepend(boringButton);
+boringButton.textContent = "Boring Mode";
+
+boringButton.addEventListener('click', () => {
+    mode = 1;
+    newGrid();
+});
+
+//Creates random color for mouseover event listener
+function color() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor;
+}
